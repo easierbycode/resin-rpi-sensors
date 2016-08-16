d1e8805e3c93519afbf1beb7a8761e903e1f803c
Daniel Johnson <navseeker7@gmail.com>
2016-08-16 15:57:57 -0700

    rename sense -> sensor

--- lux.js
+++ lux.js
@@ -2,16 +2,16 @@ var Q       = require( 'q' );
 var TSL2561 = require( 'sensor_tsl2561' );
 
 
-var sense = new TSL2561();
+var sensor = new TSL2561();
 
 var initSensor = Q.defer();
-sense.init( ( err, val ) => { if ( !err ) initSensor.resolve() })
+sensor.init( ( err, val ) => { if ( !err ) initSensor.resolve() })
 
 module.exports = () => {
   var deferred = Q.defer();
 
   initSensor.promise.then( ( err, val ) => {
-    sense.getLux( ( err, luxVal ) => {
+    sensor.getLux( ( err, luxVal ) => {
       if ( !err )  deferred.resolve( { lux:luxVal } )
     })
   });
